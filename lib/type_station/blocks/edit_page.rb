c08debb4683947e64c23bd1b85738b33af4d9cab
Richard Adams <richard.adams@thinkidea.co.uk>
2015-02-20 14:47:20 +0000

    Should treat values that are symbols as strings

--- lib/type_station/blocks/edit_page.rb
+++ lib/type_station/blocks/edit_page.rb
@@ -17,7 +17,7 @@ module TypeStation
       def tag_ts_data
         values = options[:fields].map do |field| 
           content = model.try(field[:name])
-          value = content.is_a?(String) ? content : content.try(:value)
+          value = content.is_a?(String) || content.is_a?(Symbol) ? content.to_s : content.try(:value)
           [field[:name], value] if value
         end
         super.merge({ts_values: Hash[*values.compact.flatten(1)], ts_position: model.position })
