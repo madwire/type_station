46ff910b5104525c8e40b974e0fbdfcfadaa9a04
Richard Adams <richard@madwire.co.uk>
2015-07-03 14:58:20 +0100

    added missing end to PagesController

--- app/controllers/type_station/admin/pages_controller.rb
+++ app/controllers/type_station/admin/pages_controller.rb
@@ -2,14 +2,14 @@ module TypeStation
   module Admin
     class PagesController < ::TypeStation::AdminController
 
-    def index
-      @pages = TypeStation::Page.in(status: [:draft, :published])
-      @pages = @pages.where(title: /#{params[:title]}/i) if params[:title]
-      @pages = @pages.where(path: /#{params[:path]}/i) if params[:path]
-
-      render json: { status: :success, pages: @pages }, status: :ok
+      def index
+        @pages = TypeStation::Page.in(status: [:draft, :published])
+        @pages = @pages.where(title: /#{params[:title]}/i) if params[:title]
+        @pages = @pages.where(path: /#{params[:path]}/i) if params[:path]
+  
+        render json: { status: :success, pages: @pages }, status: :ok
+      end
+    
     end
-
   end
-
 end
