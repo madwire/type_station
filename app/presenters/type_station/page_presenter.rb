6f37e157f40121d69f72fb79b0351cc72675d433
Richard Adams <richard.adams@thinkidea.co.uk>
2015-07-08 16:24:28 +0100

    title is required by default

--- app/presenters/type_station/page_presenter.rb
+++ app/presenters/type_station/page_presenter.rb
@@ -3,7 +3,7 @@ module TypeStation
     presents :page
     delegate :to_param, :title, :path, :slug, :type, :template_name, :template_name?, :visible?, :redirect_to, :redirect?, :position, to: :page
 
-    form_field :title, type: :text, label: 'Title'
+    form_field :title, type: :text, label: 'Title', required: true
 
     def children
       TypeStation::Page.where(parent_id: page.id)
