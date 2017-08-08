8a816144178f9b36d9d7a9659be0a22fd89ac7d7
Richard Adams <richard@madwire.co.uk>
2017-08-08 12:53:38 +0100

    fixed private files issue with cloudinary

--- app/controllers/type_station/file_controller.rb
+++ app/controllers/type_station/file_controller.rb
@@ -1,6 +1,8 @@
 module TypeStation
   class FileController < TypeStation::ApplicationController
 
+    IMAGE_FORMATS = ['jpg', 'jpe', 'jpeg', 'jpc', 'jp2', 'j2k', 'wdp', 'jxr', 'hdp', 'png', 'gif', 'gif', 'webp', 'webp', 'bmp', 'tif', 'tiff', 'ico', 'pdf', 'ps', 'ept', 'eps', 'eps3', 'psd', 'svg', 'ai', 'djvu', 'flif', 'tga']
+
     def download
       redirect_to private_download_url
     end
@@ -8,7 +10,11 @@ module TypeStation
     private
 
     def private_download_url
-      Cloudinary::Utils.private_download_url([params[:identifier], params[:format]].join('.'), params[:format], resource_type: :raw, attachment: true, expires_at: 1.minute.from_now)
+      if IMAGE_FORMATS.include?(params[:format].downcase)
+        Cloudinary::Utils.private_download_url(params[:identifier], params[:format], resource_type: :image, attachment: true, expires_at: 1.minute.from_now)
+      else
+        Cloudinary::Utils.private_download_url([params[:identifier], params[:format]].join('.'), params[:format], resource_type: :raw, attachment: true, expires_at: 1.minute.from_now)
+      end
     end
 
   end
